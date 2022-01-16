fc11de98c08a57415475a282614480ddb09b3624
Miguel <mige.d@outlook.com>
2022-01-15 23:55:24 -0500

    set unique id for cards

--- client/src/components/StopResults.jsx
+++ client/src/components/StopResults.jsx
@@ -4,14 +4,13 @@ import { StopCard } from './StopCard';
 
 export const StopResults = (props) => {
   const stops = useSelector((state) => state.stopResults.searchResults);
-  console.log(stops);
 
   const results = stops.map((s) => {
     return s.route_stops.map((r) => {
       return (
         <StopCard
           sx={{ my: 1 }}
-          key={s.id}
+          key={s.id + r.route.route_short_name}
           stop_name={s.stop_name}
           route_id={r.route.route_short_name}
           route_name={r.route.route_long_name}
