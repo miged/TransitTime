24ecfb23f81efbc7bf321118051456fa761c3620
Shoumik Chowdhury <shoumikchowdhury@Shoumiks-Air.hitronhub.home>
2022-01-17 16:36:42 -0600

    send test data when endpoint recieves a request

--- server/routes/busLocation.js
+++ server/routes/busLocation.js
@@ -1,5 +1,10 @@
 const axios = require("axios");
+const { response } = require("express");
+const express = require("express");
+const router = express.Router();
 const GtfsRealtimeBindings = require("gtfs-realtime-bindings");
+
+let data = {};
 
 const url =
   "http://gtfs.edmonton.ca/TMGTFSRealTimeWebService/Vehicle/VehiclePositions.pb";
@@ -13,8 +18,19 @@ axios({
     const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
       response.data
     );
-    console.log(feed.entity[0]);
+    data["position"] = feed.entity[0]["vehicle"]["position"];
+    console.log(data);
+    // console.log(feed.entity[0]);
   })
   .catch(function (error) {
     console.log(error);
   });
+
+module.exports = () => {
+  router.get("/", (req, res) => {
+    console.log("Sending bus location....");
+    res.json(data);
+  });
+
+  return router;
+};
