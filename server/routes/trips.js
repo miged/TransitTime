c8b1b6d7dcfde99311b924d390bc07c235e84fa6
nyoshida44 <nyoshida44@gmail.com>
2022-01-17 13:26:25 -0500

    RouteId, StopId, and TripId in proxy server, code cleanup

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -23,44 +23,40 @@ module.exports = () => {
     request(requestSettings, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
-        let currentDate = new Date(feed.header.timestamp.low *1000)
-        let modifiedDate = currentDate.toLocaleTimeString('en-GB', { timeZone: 'America/Edmonton', hour12: false })
-        let b = modifiedDate.split(":");
-        let currentMinutes = (+b[0]) * 60 + (+b[1]);
-        console.log("Current Time:")
-        console.log(currentMinutes)
-        console.log("Bus Arrival Times:")
+        const currentDate = new Date(feed.header.timestamp.low *1000)
+        const modifiedDate = currentDate.toLocaleTimeString('en-GB', { timeZone: 'America/Edmonton', hour12: false })
+        const b = modifiedDate.split(":");
+        const currentMinutes = (+b[0]) * 60 + (+b[1]);
         const array = []
-        let stopData = feed.entity.forEach((e) => {
+        const stopData = feed.entity.forEach((e) => {
           e.tripUpdate.stopTimeUpdate.forEach((s) => {
-            if (s.stopId === "5359") {
-              let myDate = new Date(s.departure.time.low *1000)
-              let busHMS = myDate.toLocaleTimeString('en-GB', { timeZone: 'America/Edmonton', hour12: false })
-              let a = busHMS.split(':');
-              let busMinutes = (+a[0]) * 60 + (+a[1]);
-              let minutesUntilBus = busMinutes - currentMinutes
+            if (s.stopId === "5359" && e.tripUpdate.trip.routeId === "004") {
+              const myDate = new Date(s.departure.time.low *1000)
+              const busHMS = myDate.toLocaleTimeString('en-GB', { timeZone: 'America/Edmonton', hour12: false })
+              const a = busHMS.split(':');
+              const busMinutes = (+a[0]) * 60 + (+a[1]);
+              const minutesUntilBus = busMinutes - currentMinutes
+              array.stopId = s.stopId
               array.push({
-                route: e.tripUpdate.trip.routeId,
-                time: minutesUntilBus
+                stopId: s.stopId,
+                tripId: e.tripUpdate.trip.tripId,
+                routeId: e.tripUpdate.trip.routeId,
+                time: minutesUntilBus,
+                vehicleID: e.tripUpdate.vehicle.id
               });
             }
           })
         })
-        let modifiedArray = array.filter((arr) => {
+        const modifiedArray = array.filter((arr) => {
           return arr.time > 0
         })
         modifiedArray.sort((a,b) => a.time - b.time)
-        console.log(modifiedArray)
+        //console.log(modifiedArray)
         return res.json(JSON.stringify(modifiedArray))
-        //let arrival = feed.entity[0]["tripUpdate"]["stopTimeUpdate"][0]["departure"]["time"]["low"]
-        //const myDate = new Date( arrival *1000);
-        //console.log(myDate.toLocaleString())
       }
     });
   });
-
   return router;
-
 };
 
 
