8798a89bc587319aed965427eb6178ab662b3b68
Shoumik Chowdhury <shoumikchowdhury@Shoumiks-Air.hitronhub.home>
2022-01-17 16:56:57 -0600

    fix lagging data

--- server/routes/busLocation.js
+++ server/routes/busLocation.js
@@ -1,35 +1,32 @@
 const axios = require("axios");
-const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const GtfsRealtimeBindings = require("gtfs-realtime-bindings");
 
-let data = {};
-
-const url =
-  "http://gtfs.edmonton.ca/TMGTFSRealTimeWebService/Vehicle/VehiclePositions.pb";
-
-axios({
-  method: "GET",
-  url: url,
-  responseType: "arraybuffer",
-})
-  .then(function (response) {
-    const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
-      response.data
-    );
-    data["position"] = feed.entity[0]["vehicle"]["position"];
-    console.log(data);
-    // console.log(feed.entity[0]);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
 module.exports = () => {
+  let data = {};
+
   router.get("/", (req, res) => {
-    console.log("Sending bus location....");
-    res.json(data);
+    const url =
+      "http://gtfs.edmonton.ca/TMGTFSRealTimeWebService/Vehicle/VehiclePositions.pb";
+
+    axios({
+      method: "GET",
+      url: url,
+      responseType: "arraybuffer",
+    })
+      .then((res) => {
+        const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+          res.data
+        );
+        data["position"] = feed.entity[0]["vehicle"]["position"];
+        console.log(data);
+        // console.log(feed.entity[0]);
+      })
+      .then(() => res.json(data))
+      .catch(function (error) {
+        console.log(error);
+      });
   });
 
   return router;
