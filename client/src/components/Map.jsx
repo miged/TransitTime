3a8b83e20c0451465aa4f4b969117c929197b607
Shoumik Chowdhury <shoumikchowdhury@Shoumiks-Air.hitronhub.home>
2022-01-18 12:46:31 -0600

    add test busStop coordinates, change bus, stop icons

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -3,70 +3,67 @@ import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import axios from "axios";
 
-// let data = [
-//   { lat: 53.53055, lon: -113.50073 },
-//   { lat: 53.53062, lon: -113.50015 },
-//   { lat: 53.53075, lon: -113.49958 },
-//   { lat: 53.53093, lon: -113.49914 },
-//   { lat: 53.53111, lon: -113.49881 },
-//   { lat: 53.53128, lon: -113.49848 },
-// ];
-
 const Map = () => {
-  let [coordinate, setCoordinate] = useState({
-    lat: 53.53051,
-    lon: -113.50109,
+  let [stopCoordinate, setStopCoordinate] = useState({
+    lat: 53.53414,
+    lon: -113.50254,
+  });
+  let [busCoordinate, setBusCoordinate] = useState({
+    lat: 53.5341,
+    lon: -113.50227,
   });
 
   let [count, setCount] = useState(0);
-  // useEffect(() => {
-  //   let i = 0;
-  //   setInterval(() => {
-  //     setCoordinate(data[i]);
-  //     if (i === 5) {
-  //       i = -1;
-  //     }
-  //     i++;
-  //   }, 3000);
-  // }, []);
 
   const busIcon = L.icon({
-    iconUrl: "./assets/bus-stop.png",
-    iconSize: [24.125, 32],
-    iconAnchor: [12.0625, 32],
+    iconUrl: "./assets/bus_pos.png",
+    iconSize: [24.4, 28],
+    iconAnchor: [12.2, 28],
+  });
+
+  const stopIcon = L.icon({
+    iconUrl: "./assets/stop_icon.png",
+    iconSize: [18, 18],
+    iconAnchor: [9, 9],
   });
 
   useEffect(() => {
-    axios.get("api/busLocation").then((res) => {
-      // let data = JSON.parse(res.data);
-      let data = res.data.position;
-      console.log(data);
-      console.log(data.latitude);
-      setCoordinate((prev) => ({
-        ...prev,
-        lat: data.latitude,
-        lon: data.longitude,
-      }));
-    });
+    axios
+      .get("api/busLocation")
+      .then((res) => {
+        let data = res.data.position;
+        setBusCoordinate((prev) => ({
+          ...prev,
+          lat: data.latitude,
+          lon: data.longitude,
+        }));
+      })
+      .catch((err) => console.log(err));
   }, [count]);
 
   return (
     <>
       <MapContainer
         id="map"
-        center={[coordinate.lat, coordinate.lon]}
-        zoom={18}
+        center={[stopCoordinate.lat, stopCoordinate.lon]}
+        zoom={17}
         scrollWheelZoom={false}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker icon={busIcon} position={[coordinate.lat, coordinate.lon]}>
+        <Marker
+          icon={busIcon}
+          position={[busCoordinate.lat, busCoordinate.lon]}
+        >
           <Popup>Last updated 30s ago.</Popup>
         </Marker>
 
-        <Marker position={[53.53062, -113.50018]}>
+        <Marker
+          icon={stopIcon}
+          position={[stopCoordinate.lat, stopCoordinate.lon]}
+        >
           <Popup>Bus Stop #111</Popup>
         </Marker>
       </MapContainer>
