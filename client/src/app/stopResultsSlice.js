106322c6ff94605bfbd94ec0bf1ef4ac3c8de368
Miguel <mige.d@outlook.com>
2022-01-18 14:32:15 -0500

    add favourites reducer

--- client/src/app/stopResultsSlice.js
+++ client/src/app/stopResultsSlice.js
@@ -5,6 +5,7 @@ export const stopResultsSlice = createSlice({
   initialState: {
     searchResults: [],
     autocompleteResults: [],
+    favouriteResults: [],
   },
   reducers: {
     setSearchResults: (state, action) => {
@@ -13,11 +14,14 @@ export const stopResultsSlice = createSlice({
     setAutocompleteResults: (state, action) => {
       state.autocompleteResults = action.payload;
     },
+    setFavouriteResults: (state, action) => {
+      state.favouriteResults = action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setSearchResults, setAutocompleteResults } =
+export const { setSearchResults, setAutocompleteResults, setFavouriteResults } =
   stopResultsSlice.actions;
 
 export default stopResultsSlice.reducer;
