278134623bd9ae15951fe868e1a62726a3895f03
Shoumik Chowdhury <shoumikchowdhury@Shoumiks-Air.hitronhub.home>
2022-01-21 00:27:54 -0600

    add functioning lastUpdated tooltip on bus. Set initial view onload to bus location

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -15,6 +15,7 @@ export const Map = (props) => {
     lat: 53.5341,
     lon: -113.50227,
   });
+  let [timeUpdate, setTimeUpdate] = useState(null);
 
   let stopId = props.stop_id;
   let busId = props.vehicle_id;
@@ -34,8 +35,9 @@ export const Map = (props) => {
   });
 
   function SetView(lat, lon) {
-    map.panTo([lat, lon]);
-
+    if (map) {
+      map.panTo([lat, lon]);
+    }
     return null;
   }
 
@@ -44,7 +46,7 @@ export const Map = (props) => {
       .get(`api/stopLocation/${stopId}`)
       .then((res) => {
         if (Object.keys(res.data).length === 0) {
-          console.log("NO STOPS FOUND");
+          console.log("NO STOPS FOUND!");
         }
         if (Object.keys(res.data).length !== 0) {
           setStopCoordinate((prev) => ({
@@ -52,29 +54,54 @@ export const Map = (props) => {
             lat: Number(res.data.stop_lat),
             lon: Number(res.data.stop_lon),
           }));
-          console.log("Set STOP to:", stopCoordinate);
+          console.log("Set stop COORD to:", stopCoordinate);
         }
       })
       .catch((err) => console.log(err));
-  }, []);
 
-  useInterval(() => {
+    // Initial request busLocation
     axios
       .get(`api/busLocation/${busId}`)
       .then((res) => {
         let data = res.data.position;
-        console.log("Bus COORD: ", data);
+        console.log("Recieved from api: \nBus COORD: ", data);
         if (data) {
           setBusCoordinate((prev) => ({
             ...prev,
             lat: data.latitude,
             lon: data.longitude,
           }));
-          SetView(data.latitude, data.longitude);
         }
+        // Get time last updated
+        let timeDiff = Math.floor(Date.now() / 1000) - res.data.time.low;
+        setTimeUpdate((prev) => timeDiff);
       })
       .catch((err) => console.log(err));
-  }, 15000);
+  }, []);
+
+  useEffect(() => {
+    SetView(busCoordinate.lat, busCoordinate.lon);
+  }, [busCoordinate]);
+
+  useInterval(() => {
+    axios
+      .get(`api/busLocation/${busId}`)
+      .then((res) => {
+        let data = res.data.position;
+        console.log("Recieved from api: \nBus COORD: ", data);
+        if (data) {
+          setBusCoordinate((prev) => ({
+            ...prev,
+            lat: data.latitude,
+            lon: data.longitude,
+          }));
+        }
+        // Get time last updated
+        let timeDiff = Math.floor(Date.now() / 1000) - res.data.time.low;
+        setTimeUpdate((prev) => timeDiff);
+      })
+      .catch((err) => console.log(err));
+  }, 10000);
 
   return (
     <>
@@ -84,7 +111,7 @@ export const Map = (props) => {
           setMap(map);
         }}
         center={[stopCoordinate.lat, stopCoordinate.lon]}
-        zoom={16}
+        zoom={15}
         scrollWheelZoom={false}
       >
         <TileLayer
@@ -101,14 +128,14 @@ export const Map = (props) => {
             opacity={0.8}
             offset={point}
           >
-            30s ago.
+            Updated: {timeUpdate}s ago.
           </Tooltip>
         </Marker>
         <Marker
           icon={stopIcon}
           position={[stopCoordinate.lat, stopCoordinate.lon]}
         >
-          <Popup>Stop: {stopId}</Popup>
+          <Popup>Stop: #{stopId}</Popup>
         </Marker>
       </MapContainer>
     </>
