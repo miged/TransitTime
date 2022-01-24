2cefeb76c8a24b2557041e3e8d2af5febc28a15d
Miguel <mige.d@outlook.com>
2022-01-24 18:13:29 -0500

    add mapbox attribution

--- client/src/components/NearbyMap.tsx
+++ client/src/components/NearbyMap.tsx
@@ -31,7 +31,7 @@ export const NearbyMap = () => {
         scrollWheelZoom={false}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="https://www.mapbox.com/">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           url={`https://api.mapbox.com/styles/v1/shoumik2022/${style}/tiles/256/{z}/{x}/{y}@2x?access_token=${mapKey}`}
         />
         {stops}
