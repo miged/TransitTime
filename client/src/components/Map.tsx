0afd43104798e24dd4187a5ea93520d4f6d07369
Miguel <mige.d@outlook.com>
2022-01-29 00:12:30 -0500

    cleanup map

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,75 +1,82 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useEffect } from "react";
-import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
-import useInterval from "react-useinterval";
+import { useState, useEffect } from 'react';
+import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from 'react-leaflet';
+import useInterval from 'react-useinterval';
 
-import { fetchBus, fetchRoute, fetchStop } from "../app/fetchData";
+import { fetchBus, fetchRoute, fetchStop } from '../app/fetchData';
 
-export const Map = (props) => {
-  let [map, setMap] = useState(null);
-  let [stopCoordinate, setStopCoordinate] = useState({
+export interface Props {
+  stop_code: string;
+  vehicle_id: string;
+  agency: string;
+  route_id: string;
+  route_num: string;
+}
+
+export const Map = (props: Props) => {
+  const [map, setMap] = useState<any>(null);
+  const [stopCoordinate, setStopCoordinate] = useState({
     lat: 53.53414,
     lon: -113.50254,
   });
-  let [busCoordinate, setBusCoordinate] = useState({
+  const [busCoordinate, setBusCoordinate] = useState({
     lat: 53.5341,
     lon: -113.50227,
   });
-  let [timeUpdate, setTimeUpdate] = useState(null);
-  let [routeMarkers, setRouteMarkers] = useState([]);
+  const [timeUpdate, setTimeUpdate] = useState(null);
+  const [routeMarkers, setRouteMarkers] = useState([]);
 
-  let stopId = props.stop_code;
-  let busId = props.vehicle_id;
-  let routeId = "";
-  let agency = "";
-  if (props.agency === "o-c3x-edmontontransitservice") {
-    agency = "ets";
+  let routeId = '';
+  let agency = '';
+  if (props.agency === 'o-c3x-edmontontransitservice') {
+    agency = 'ets';
     routeId = props.route_id;
-  } else if (props.agency === "o-dpz8-ttc") {
-    agency = "ttc";
+  } else if (props.agency === 'o-dpz8-ttc') {
+    agency = 'ttc';
     routeId = props.route_num;
   }
 
   // Mapbox config
   const mapKey = process.env.REACT_APP_MAPBOX_KEY;
-  let style = "ckyqkh1f811fy14k876mhrntc";
+  const style = 'ckyqkh1f811fy14k876mhrntc';
 
   // Define icons
   const point = L.point(0, -18);
-
   const busIcon = L.icon({
-    iconUrl: "./assets/bus-white.png",
+    iconUrl: './assets/bus-white.png',
     iconSize: [26, 26],
     iconAnchor: [15, 15],
   });
-
   const stopIcon = L.icon({
-    iconUrl: "./assets/stop_icon.png",
+    iconUrl: './assets/stop_icon.png',
     iconSize: [18, 18],
     iconAnchor: [9, 9],
   });
 
   useEffect(() => {
-    fetchStop(stopId, agency, setStopCoordinate);
+    fetchStop(props.stop_code, agency, setStopCoordinate);
     fetchRoute(routeId, agency, setRouteMarkers);
   }, []);
 
   useInterval(() => {
-    fetchBus(busId, agency, setBusCoordinate, setTimeUpdate);
+    fetchBus(props.vehicle_id, agency, setBusCoordinate, setTimeUpdate);
   }, 10000);
 
   return (
     <>
       <MapContainer
         id="map"
-        whenCreated={(map) => {
+        whenCreated={(map: any) => {
           setMap(map);
-          fetchBus(busId, agency, setBusCoordinate, setTimeUpdate).then(
-            (data) => {
-              map.panTo([data.latitude, data.longitude]);
-            }
-          );
+          fetchBus(
+            props.vehicle_id,
+            agency,
+            setBusCoordinate,
+            setTimeUpdate
+          ).then((data) => {
+            map.panTo([data.latitude, data.longitude]);
+          });
         }}
         center={[busCoordinate.lat, busCoordinate.lon]}
         zoom={14}
@@ -92,7 +99,7 @@ export const Map = (props) => {
         >
           <Tooltip
             permanent={true}
-            direction={"top"}
+            direction={'top'}
             opacity={0.8}
             offset={point}
           >
@@ -110,7 +117,7 @@ export const Map = (props) => {
             },
           }}
         >
-          <Popup>Your stop: #{stopId}</Popup>
+          <Popup>Your stop: #{props.stop_code}</Popup>
         </Marker>
         {routeMarkers}
       </MapContainer>
